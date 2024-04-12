d834799a0824b51b964147232e7c91cf520e0ffe
stephen-osullivan <31805109+stephen-osullivan@users.noreply.github.com>
2024-04-12 11:50:35 +0100

    Delete 03_huggingface/download_llama2.py

--- 03_huggingface/download_llama2.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-
-load_dotenv()
-model_id = "meta-llama/Llama-2-7b-chat-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_id, return_tensorts='pt', add_special_tokens=False)
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map='cuda', torch_dtype = torch.float16)
